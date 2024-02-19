64a225d174d5789619ce90e3b9f01136b052515d
MarcelGiemza <u80_margie_waw@technischools.com>
2024-02-19 13:39:36 +0100

    updates

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 from flask.views import MethodView
-from flask import Flask, request
+from flask import Flask, request, Response
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from database import users
-from enum import Enum
+
+
+OK = 200
+CREATED = 201
+NO_CONTENT = 204
+WRONG_REQUEST = 400
+NOT_FOUND = 404
+
 
 def merge_dicts(dict1: dict, dict2: dict) -> dict:
     """Merges two dicts toghether"""
     return {**dict1, **dict2}
 
+
 app = Flask(__name__)
 
 
 class ItemAPI(MethodView):
-    def __init__(self, db, model):
+    def __init__(self, db, model, name):
         self.db = db
         self.model = model
+        self.name = name
 
     def _get(self, id):
-        user = users.get(id, None)
-        if not user:
+        item = self.db.get(id, None)
+        if not item:
             return {"error": "id error", "data": None}
-        user.update({"id": id})
-        return {"error": None, "data": user}
+        item.update({"id": id})
+        return {"error": None, "data": item}
 
     def get(self, id):
-        return self._get(id)
+        item = self._get(id)
+        if item["error"]:
+            return Response(status=NOT_FOUND)
+        return {self.name: item["data"]}
 
     def patch(self, id):
+        # if
 
         user = self._get(id)
 
 
 class GroupAPI(MethodView):
-    def __init__(self, db, model):
+    def __init__(self, db, model, name):
         self.db = db
         self.model = model
+        self.name = name
 
     def get(self):
         return {"error": None, "data": list(map(lambda user: merge_dicts({"id": user[0]}, user[1]), list(users.items())))}
@@ -86,8 +100,8 @@ class _User:
 User = _User()
 
 
-user_item_api = ItemAPI.as_view("user-item", users, User)
-user_group_api = GroupAPI.as_view("user-group", users, User)
+user_item_api = ItemAPI.as_view("user-item", users, User, "user")
+user_group_api = GroupAPI.as_view("user-group", users, User, "users")
 app.add_url_rule(f"/users/<int:id>", view_func=user_item_api)
 app.add_url_rule(f"/users/", view_func=user_group_api)
 
